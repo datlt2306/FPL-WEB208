3218b761ebba5a31a6f1ada28dfc940f77d379bb
datlt2306 <datlt2306@gmail.com>
2024-09-14 15:21:48 +0700

    WD18402 - Buổi 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CounterComponent } from './components/counter/counter.component';
+// decorator
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular';
 }
+// cli
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  count: number = 0;
+  increment() {
+    this.count++;
+  }
+  decrement() {
+    this.count--;
+  }
+  increase(value: number) {
+    this.count += value;
+  }
+}
