f35249965ce245a62db22a6d6970536d3cbaedc3
datlt2306 <datlt2306@gmail.com>
2024-09-19 13:40:56 +0700

    WD18401 - Buổi 5

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IProduct } from '../../interfaces/product';
 import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IProduct } from '../../interfaces/product';
+import { ProductService } from '../../services/product.service';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductItemComponent } from '../product-item/product-item.component';
 
@@ -14,98 +15,11 @@ import { ProductItemComponent } from '../product-item/product-item.component';
 })
 export class ProductListComponent {
   selectedProduct!: IProduct;
-  products: IProduct[] = [
-    {
-      "id": 1,
-      "name": "Smartphone Pro X",
-      "code": "SPX-001",
-      "available": true,
-      "price": 999.99,
-      "starRating": 4.8,
-      "imageUrl": "https://picsum.photos/id/1/200/300"
-    },
-    {
-      "id": 2,
-      "name": "Wireless Headphones",
-      "code": "WH-202",
-      "available": false,
-      "price": 199.99,
-      "starRating": 4.5,
-      "imageUrl": "https://picsum.photos/id/2/200/300"
-    },
-    {
-      "id": 3,
-      "name": "Gaming Laptop GT",
-      "code": "GLGT-500",
-      "available": true,
-      "price": 1499.99,
-      "starRating": 4.7,
-      "imageUrl": "https://picsum.photos/id/3/200/300"
-    },
-    {
-      "id": 4,
-      "name": "4K Monitor Ultra",
-      "code": "4KMU-120",
-      "available": true,
-      "price": 399.99,
-      "starRating": 4.6,
-      "imageUrl": "https://picsum.photos/id/4/200/300"
-    },
-    {
-      "id": 5,
-      "name": "Mechanical Keyboard",
-      "code": "MK-340",
-      "available": true,
-      "price": 89.99,
-      "starRating": 4.2,
-      "imageUrl": "https://picsum.photos/id/5/200/300"
-    },
-    {
-      "id": 6,
-      "name": "Smartwatch Fit",
-      "code": "SWF-111",
-      "available": false,
-      "price": 149.99,
-      "starRating": 4.4,
-      "imageUrl": "https://picsum.photos/id/6/200/300"
-    },
-    {
-      "id": 7,
-      "name": "Bluetooth Speaker",
-      "code": "BTS-600",
-      "available": true,
-      "price": 59.99,
-      "starRating": 4.3,
-      "imageUrl": "https://picsum.photos/id/7/200/300"
-    },
-    {
-      "id": 8,
-      "name": "Portable SSD 1TB",
-      "code": "PSSD-1T",
-      "available": true,
-      "price": 129.99,
-      "starRating": 4.9,
-      "imageUrl": "https://picsum.photos/id/8/200/300"
-    },
-    {
-      "id": 9,
-      "name": "Drone Max Air",
-      "code": "DMA-900",
-      "available": false,
-      "price": 899.99,
-      "starRating": 4.8,
-      "imageUrl": "https://picsum.photos/id/9/200/300"
-    },
-    {
-      "id": 10,
-      "name": "Action Camera 4K",
-      "code": "AC-4K-220",
-      "available": true,
-      "price": 299.99,
-      "starRating": 4.5,
-      "imageUrl": "https://picsum.photos/id/10/200/300"
-    }
-  ]
+  myName!: string;
+  products!: IProduct[]
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProduct();
+  }
   onRemove(id: number) {
     const confirm = window.confirm(`Are you sure you want to remove product with id ${id}?`);
     if (confirm) {
@@ -119,3 +33,5 @@ export class ProductListComponent {
     console.log(id);
   }
 }
+
+// ng g s services/product
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+  getProduct() {
+    return [
+      {
+        "id": 1,
+        "name": "Smartphone Pro X",
+        "code": "SPX-001",
+        "available": true,
+        "price": 999.99,
+        "starRating": 4.8,
+        "imageUrl": "https://picsum.photos/id/1/200/300"
+      },
+      {
+        "id": 2,
+        "name": "Wireless Headphones",
+        "code": "WH-202",
+        "available": false,
+        "price": 199.99,
+        "starRating": 4.5,
+        "imageUrl": "https://picsum.photos/id/2/200/300"
+      },
+      {
+        "id": 3,
+        "name": "Gaming Laptop GT",
+        "code": "GLGT-500",
+        "available": true,
+        "price": 1499.99,
+        "starRating": 4.7,
+        "imageUrl": "https://picsum.photos/id/3/200/300"
+      },
+      {
+        "id": 4,
+        "name": "4K Monitor Ultra",
+        "code": "4KMU-120",
+        "available": true,
+        "price": 399.99,
+        "starRating": 4.6,
+        "imageUrl": "https://picsum.photos/id/4/200/300"
+      },
+      {
+        "id": 5,
+        "name": "Mechanical Keyboard",
+        "code": "MK-340",
+        "available": true,
+        "price": 89.99,
+        "starRating": 4.2,
+        "imageUrl": "https://picsum.photos/id/5/200/300"
+      },
+      {
+        "id": 6,
+        "name": "Smartwatch Fit",
+        "code": "SWF-111",
+        "available": false,
+        "price": 149.99,
+        "starRating": 4.4,
+        "imageUrl": "https://picsum.photos/id/6/200/300"
+      },
+      {
+        "id": 7,
+        "name": "Bluetooth Speaker",
+        "code": "BTS-600",
+        "available": true,
+        "price": 59.99,
+        "starRating": 4.3,
+        "imageUrl": "https://picsum.photos/id/7/200/300"
+      },
+      {
+        "id": 8,
+        "name": "Portable SSD 1TB",
+        "code": "PSSD-1T",
+        "available": true,
+        "price": 129.99,
+        "starRating": 4.9,
+        "imageUrl": "https://picsum.photos/id/8/200/300"
+      },
+      {
+        "id": 9,
+        "name": "Drone Max Air",
+        "code": "DMA-900",
+        "available": false,
+        "price": 899.99,
+        "starRating": 4.8,
+        "imageUrl": "https://picsum.photos/id/9/200/300"
+      },
+      {
+        "id": 10,
+        "name": "Action Camera 4K",
+        "code": "AC-4K-220",
+        "available": true,
+        "price": 299.99,
+        "starRating": 4.5,
+        "imageUrl": "https://picsum.photos/id/10/200/300"
+      }
+    ]
+  }
+}
