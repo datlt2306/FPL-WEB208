ad1a99b8a56f2840620ea95670c5b43bde5a3f1b
datlt2306 <datlt2306@gmail.com>
2024-09-24 12:28:02 +0700

    WD18401 - Buổi 7
    - Tạo router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminProductsPageComponent } from './pages/admin/admin-products-page/admin-products-page.component';
+import { LayoutWebsiteComponent } from './layouts/layout-website/layout-website.component';
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: LayoutWebsiteComponent, children: [
+            { path: "", component: HomeComponent },
+            { path: "about", component: AboutComponent },
+        ]
+    },
+    { path: 'admin', component: LayoutAdminComponent },
+];
---
+++ src/app/pages/admin/admin-products-page/admin-products-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-products-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-products-page.component.html',
+  styleUrl: './admin-products-page.component.css'
+})
+export class AdminProductsPageComponent {
+
+}
