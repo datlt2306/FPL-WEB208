217c5358cf549a12277f13f386a74c29f7dfddf3
datlt2306 <datlt2306@gmail.com>
2025-01-08 13:03:17 +0700

    Refactor ProductList component to display student names and integrate with app component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-
+  @Input() students: any[] = []
+  removeStudent(id: number) {
+    console.log(id);
+  }
 }
